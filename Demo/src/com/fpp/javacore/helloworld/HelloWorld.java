9c61ef23afb61cf09ff64b624ab87e8da122e02c
ping6ping <1246678352@qq.com>
2017-03-20 09:21:14 +0800

    this is daliang
    Signed-off-by: ping6ping <1246678352@qq.com>

---
+++ Demo/src/com/fpp/javacore/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.fpp.javacore.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("张玉良你不能再这么帅下去了");
+		
+	}
+
+}
